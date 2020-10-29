f495bac407b09b3bcd75f23c85334bdadfd01962
Ben Goodwin <70025930+bgoodwinSMU@users.noreply.github.com>
2020-10-29 14:18:34 -0600

    Update hw10.r

--- Unit11/hw10.r
+++ Unit11/hw10.r
@@ -6,6 +6,7 @@
 #Question 1
 #libraries
 library(ggplot2)
+library(investr)
 
 #read data
 birdDat <- read.csv("birdDat.csv")
@@ -18,6 +19,8 @@ plot(birdDat$Mass,birdDat$Tcell)
 #make da regression model
 birdLm <- lm(Tcell~Mass,data = birdDat)
 summary(birdLm)
+plot(birdLm)
+
 
 plot(birdDat$Mass,birdDat$Tcell , ylim=c(0, 0.7), xlab="Mass", ylab="Tcell", main="Regression, CI, PI")
 abline(birdLm, col="lightblue")
@@ -52,6 +55,19 @@ predict(birdLm, newdata = new.dat, interval = 'confidence', level = .99)
 predict(birdLm, newdata = new.dat, interval = 'prediction', level = .99)
 
 #Calibration 
+#I part I
+calibrate(birdLm,y0=0.3,interval = "inversion",mean.response = TRUE,level =0.99)
+#Part II
+calibrate(birdLm,y0=0.3,interval = "inversion",level =0.99)
 
-(cal <- calibrate(birdLm, y0 = 3, interval = "inversion"))
+
+#Hist
+h <- hist(birdLm$residuals, breaks = 10, density = 10,
+          col = "lightgray",main = "Residuals") 
+xfit <- seq(min(birdLm$residuals), max(birdLm$residuals), length = 40) 
+yfit <- dnorm(xfit, mean = mean(birdLm$residuals), sd = sd(birdLm$residuals)) 
+yfit <- yfit * diff(h$mids[1:2]) * length(birdLm$residuals) 
+
+lines(xfit, yfit, col = "black", lwd = 2)
+
 
